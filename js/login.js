dcca41f6199a2bc826358dfca7020f82dd81afff
MartinLast <martin.lastra@hotmail.com>
2020-08-13 16:14:06 -0300

    Submit_01

--- js/login.js
+++ js/login.js
@@ -2,13 +2,25 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 
-form.addEventListener('submit',function(event){
+document.getElementById('submit_login').addEventListener("click",function(event){
     event.preventDefault();
+    document.getElementById("advertencia-user").innerHTML =''
+    document.getElementById("advertencia-pass").innerHTML =''
+    if (username.value.trim().length === 0){
+      document.getElementById("advertencia-user").innerHTML = 'Por favor ingrese un usuario válido.'
+    }
+    else { 
+        if (password.value.trim().length===0){
+          document.getElementById("advertencia-pass").innerHTML = 'Por favor ingrese una contraseña válida.'
+        }
+        else {
+      localStorage.setItem('user',username.value);
+      location.href="index.html"}
+    }
     //let users = {
       //  usuario: username.value,
     //};
-    localStorage.setItem('user',username.value);
-    location.href="index.html"
+    
 });
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
