f0a7dcceda6ece994d09f2ad1269c763edf35689
MartinLast <martin.lastra@hotmail.com>
2020-10-09 19:41:31 -0300

    Improvements01
    added two different functions for calculating partial and total price.
    added functionality to eliminate articles from the cart.

--- js/cart.js
+++ js/cart.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 function showCart(arrayC)
 {
+    i=0;
     for (let prod of arrayC.articles){
 
         document.getElementById("cartList").innerHTML+=`
@@ -19,15 +20,26 @@ function showCart(arrayC)
         <th scope="row"><img class="w-25" src="${prod.src}" alt=""></th>
         <td class="pt-5">${prod.name}</td>
         <td class="pt-5" id="price${i}">${(prod.currency=="USD")? (prod.unitCost*40):(prod.unitCost)}</td>
-        <td class="pt-5"> <input type="number" id="count${i}" value="${prod.count}" min="0" oninput="calculatePrice(${i})" style="width:50px"> </td>
+        <td class="pt-5"> <input type="number" id="count${i}" value="${prod.count}" min="0" oninput="calculatePartial(${i})" style="width:50px"> </td>
         <td class="pt-5" id="subtotal${i}">${(prod.currency=="USD")? (prod.unitCost*40):(prod.unitCost)*prod.count}</td>
+        <td class="pt-5"> <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg" onclick="eliminate(${i})">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg></td>
       </tr>`
       i++;
     }
     document.getElementById("cartList").innerHTML+=`<tr>
+    <th scope="row"><img class="w-25 border" src="" alt=""></th>
+    <td class="pt-5"></td>
+    <td class="pt-5"></td>
+    <th class="pt-5">IMPORTE:</th>
+    <td class="pt-5" id="importe"></td>
+  </tr>
+  <tr>
       <th scope="row"><img class="w-25 border" src="" alt=""></th>
       <td class="pt-5"></td>
-      <td class="pt-5"><select id="envio" onchange="calculatePrice(0)">
+      <td class="pt-5"><select id="envio" onchange="calculateTotal()">
         <option value="0">Sin envio (+0%) </option>
         <option value="5">Envio Standard (+5%)</option>
         <option value="7">Envio Express (+7%)</option>
@@ -45,19 +57,37 @@ function showCart(arrayC)
     </tr>
     
     `;
-    calculatePrice(i-1);
+    calculateTotal();
 }
 
-function calculatePrice(a)
+function calculatePartial(a)
 {
     let precio=document.getElementById(`price${a}`).innerHTML;
     let cantidad=document.getElementById(`count${a}`).value;
     document.getElementById(`subtotal${a}`).innerHTML=`${parseInt(precio)*parseInt(cantidad)}`;
+    calculateTotal();
+}
+function calculateTotal(){
     let sum=0;
     for(j=0;j<i;j++){
         sum +=parseInt((document.getElementById(`subtotal${j}`).innerHTML));
     }
     let costoEnvio=(document.getElementById("envio").value);
-    document.getElementById("totalEnvio").innerHTML=`$${Math.round(costoEnvio/100.0*sum)}`
+    document.getElementById("importe").innerHTML=`$${sum}`;
+    document.getElementById("totalEnvio").innerHTML=`$${Math.round(costoEnvio/100.0*sum)}`;
     document.getElementById("total").innerHTML=`$${Math.round(sum*(1+costoEnvio/100))}`;
+};
+function eliminate(a){
+  cartQuantity=parseInt(document.getElementById('cartQTYDisplay').innerHTML);
+  if (arrayCart.articles.length>1){
+    arrayCart.articles.splice(a,1);
+    document.getElementById("cartList").innerHTML=``
+    showCart(arrayCart);
+    document.getElementById('cartQTYDisplay').innerHTML=`${cartQuantity-1}`;
+  }
+  else{
+    document.getElementById("cartList").innerHTML=``;
+    document.getElementById('cartQTYDisplay').innerHTML=`0`;
+  }
+
 }
